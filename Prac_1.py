8276e4a3927ad0938e6ef12f0b57de563f15d054
InWoo <iwoojun@gmail.com>
2019-04-08 18:36:50 +0100

    Final Prac 1 (with comments)

--- Prac_1.py
+++ Prac_1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Names: In Woo Jun #hi
+Names: In Woo Jun
 Student Number: JNXINW001
 Prac: Prac1
 Date: 23/07/2019
@@ -46,56 +46,54 @@ def LED3off(): #function to turn off LED3
 LEDcounter = 0 #create variable LEDcounter
 str_bin_conv = 0 #create variable str_bin_conv
 
-def switch1_callback(channel):
+def switch1_callback(channel): #function when switch1 (increment) is pressed
     global LEDcounter
     global str_bin_conv
-    if LEDcounter < 7:
+    if LEDcounter < 7: #increment by 1 until LEDcounter reaches 7
         LEDcounter += 1
     else:
-        LEDcounter = 0
-    bin_conv = bin(LEDcounter)[2:].zfill(3)
-    str_bin_conv = str(bin_conv)
-    #print(str_bin_conv)
+        LEDcounter = 0 #when LEDcounter reaches its maximum, restart at 0
+    bin_conv = bin(LEDcounter)[2:].zfill(3) #convert LEDcounter to binary, then cut off the "0b" by slicing, and then fill the number by keeping it 3 digits
+    str_bin_conv = str(bin_conv) #convert bin_conv to string
 
-    if str_bin_conv[:1] == "1":
+    if str_bin_conv[:1] == "1": #check if first digit is 1, if true then turn on LED 1
         LED1on()
-    else:
+    else: #if not, turn off LED 1
         LED1off()
 
-    if str_bin_conv[1:2] == "1":
+    if str_bin_conv[1:2] == "1": #check if first digit is 1, if true then turn on LED 2
         LED2on()
-    else:
+    else: #if not, turn off LED 2
         LED2off()
 
-    if str_bin_conv[2:3] == "1":
+    if str_bin_conv[2:3] == "1": #check if first digit is 1, if true then turn on LED 3
         LED3on()
-    else:
+    else: #if not, turn off LED 3
         LED3off()
 
-def switch2_callback(channel):
+def switch2_callback(channel): #function when switch2 (decrement) is pressed
     global LEDcounter
     global str_bin_conv
-    if LEDcounter > 0:
+    if LEDcounter > 0: #decrement by 1 until LEDcounter reaches 0
         LEDcounter -=1
-    else:
+    else: #when LEDcounter reaches its minimum, restart at 7
         LEDcounter = 7
-    bin_conv = bin(LEDcounter)[2:].zfill(3)
-    str_bin_conv = str(bin_conv)
-    #print(str_bin_conv)
+    bin_conv = bin(LEDcounter)[2:].zfill(3) #convert LEDcounter to binary, then cut off the "0b" by slicing, and then fill the number by keeping it 3 digits
+    str_bin_conv = str(bin_conv) #convert bin_conv to string
 
-    if str_bin_conv[:1] == "1":
+    if str_bin_conv[:1] == "1": #check if first digit is 1, if true then turn on LED 1
         LED1on()
-    else:
+    else: #if not, turn off LED 1
         LED1off()
 
-    if str_bin_conv[1:2] == "1":
+    if str_bin_conv[1:2] == "1": #check if first digit is 1, if true then turn on LED 2
         LED2on()
-    else:
+    else: #if not, turn off LED 2
         LED2off()
 
-    if str_bin_conv[2:3] == "1":
+    if str_bin_conv[2:3] == "1": #check if first digit is 1, if true then turn on LED 3
         LED3on()
-    else:
+    else: #if not, turn off LED 3
         LED3off()
 
 
@@ -105,7 +103,7 @@ GPIO.add_event_detect(27, GPIO.RISING, callback=switch2_callback, bouncetime=300
 #main function
 def main():
     switch1 = input("Press switch 1 to increment\nPress switch 2 to decrement\n")
-    GPIO.cleanup()
+    GPIO.cleanup() #clean up and reset GPIO ports
 
 # Only run the functions if
 if __name__ == "__main__":
